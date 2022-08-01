442fce3ffb4a2ea7c6daa1a1397a337fbc10d19b
Marc Enschede <marc@marcenschede.nl>
2022-08-01 22:56:45 +0200

    Completing sprint 2

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 eksctl create cluster -f eks-demo.yaml
+
+helm repo add eks https://aws.github.io/eks-charts
+
+helm install aws-load-balancer-controller \
+  eks/aws-load-balancer-controller \
+  --namespace kube-system \
+  --set clusterName=eks-demo \
+  --set serviceAccount.create=false \
+  --set serviceAccount.name=aws-load-balancer-controller
