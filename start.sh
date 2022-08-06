854888a24fb094c3aeaf752451aa3a221c6f1341
Marc Enschede <marc@marcenschede.nl>
2022-08-06 14:57:58 +0200

    Prepare for release of sprint 3

--- start.sh
+++ start.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
 eksctl create cluster -f eks-demo.yaml
-
-helm repo add eks https://aws.github.io/eks-charts
-
-helm install aws-load-balancer-controller \
-  eks/aws-load-balancer-controller \
-  --namespace kube-system \
-  --set clusterName=eks-demo \
-  --set serviceAccount.create=false \
-  --set serviceAccount.name=aws-load-balancer-controller
-
-kubectl apply -f eks-demo-app/deployment/service.yaml
-kubectl apply -f eks-demo-app/deployment/deployment.yaml
-kubectl apply -f eks-demo-app/deployment/ingress.yaml
