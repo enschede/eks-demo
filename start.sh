203a11e5644dfa1250421430fb3ce91b6cf9bca3
Marc Enschede <marc@marcenschede.nl>
2022-09-01 21:26:17 +0200

    Prepare for release

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+AWS_ACCOUNT_ID=$(aws --output json sts get-caller-identity  | jq -r '.Account')
+
 eksctl create cluster -f eks-demo.yaml
+
+kubectl create secret generic aws --namespace flux-system \
+  --from-literal=AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID \
+  --from-literal=AWS_REGION=us-west-2 \
+  --from-literal=CLUSTER_NAME=eks-demo &
+
+kubectl create secret generic github-credentials --namespace flux-system \
+  --from-literal=username=$GITHUB_USER \
+  --from-literal=password=$GITHUB_TOKEN &
--- stop.sh
+++ stop.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+flux suspend kustomization flux-system
+flux delete kustomization --silent eks-demo-app
+sleep 30
+
 eksctl delete cluster eks-demo --wait
